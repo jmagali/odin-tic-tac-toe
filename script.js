6ed16d9cdf822019b98fb2617ad62de56829bbf9
JM <jmagali.dev@gmail.com>
2025-08-27 15:42:01 -0400

    Fix bug where board and value were undeclared

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function GameBoard () {
     const length = 3;
-    board = Array.from({length}, () => Array(length).fill(Cell()));
+    const board = Array.from({length}, Array.from({length}), () => Cell());
+
 }
 
 function Cell() {
-    value = 0; // 0 = none, 1 = player 1, 2 = player 2
+    let value = 0; // 0 = none, 1 = player 1, 2 = player 2
 
     const getValue = () => {value};
 
