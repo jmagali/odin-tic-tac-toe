497161c0ccadd77650d43503a4714d6c0d5e0bed
JM <jmagali.dev@gmail.com>
2025-10-03 08:31:30 -0400

    Delete unnecessary function

--- script.js
+++ script.js
@@ -99,10 +99,6 @@ function createPlayer(playerMark) {
     };
 }
 
-function capitalizeFirstCharacter (text) {
-    return text[0].toUpperCase() + text.slice(1, text.length);
-}
-
 const controller = (function GameController() {
 
     const gameBoard = GameBoard();
